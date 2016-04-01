8f63165a8059ebab555094d51694cf450ae3e637
Hundredwz <sdwangzhuo@gmail.com>
2016-04-01 18:33:14 +0800

    finish 001 problem

---
+++ 001/activate_code.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+import random
+
+
+# 从ascii表获取随机数据
+def create_code(num, length):
+    codes = set()
+    i = 0
+    while i < num:
+        answer = ''
+        for j in range(length):
+            answer += chr(random.randint(48, 123))
+        if answer not in codes:
+            codes.add(answer)
+            i += 1
+        else:
+            continue
+    return codes
+
+
+if __name__ == '__main__':
+    res = create_code(100, 20)
+    print(res)
